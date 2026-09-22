89a952633138fcd563f3dc4ddaab8f11101af5c5
Peerapat-S <peerapatkung59@gmail.com>
2026-09-22 13:01:44 +0700

    fix to25.py

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,3 +1,7 @@
 inp: int = int(input('Enter a number less than 25\n'))
-for i in range(inp, 26):
-    print(f"Inside the loop, my variable is {i}")
+
+if inp >= 26:
+    print(f"error: {inp} is not less than 25")
+else:
+    for i in range(inp, 26):
+        print(f"Inside the loop, my variable is {i}")
